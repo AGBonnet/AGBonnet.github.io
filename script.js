a39d05674e982174567644f330339146a036f953
AGBonnet <abonnet04@gmail.com>
2023-06-03 21:22:43 +0200

    Added contact page

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const TimeDelay = 8; // Adjust the delay between each image addition
 const artInspiration = document.querySelector('.art-inspirations');
 const title = document.getElementById('title');
 const subTitle = document.getElementById('subtitle');
+const imageContainer = document.getElementById('image-container');
+const body = document.body;
 
 let imageQueue = []; // Array to store the images
 
@@ -21,11 +23,35 @@ function calculateDistance(x1, y1, x2, y2) {
   return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
-function getRandomImage() {
+function enterZoomMode(imageSrc) {
+  const zoomedImage = document.createElement('div');
+  zoomedImage.style.backgroundImage = `url('${imageSrc}')`;
+  zoomedImage.classList.add('zoomed-image');
 
+  zoomedImage.addEventListener('click', exitZoomMode);
+
+  imageContainer.innerHTML = ''; // Clear the image container
+  imageContainer.appendChild(zoomedImage); // Add the zoomed image to the container
+
+  body.classList.add('zoom-mode');
+}
+
+function exitZoomMode() {
+  imageContainer.innerHTML = ''; // Clear the image container
+
+  body.classList.remove('zoom-mode');
+
+  // Re-add the previous images
+  imageQueue.forEach(image => {
+    artInspiration.appendChild(image);
+  });
+}
+
+function getRandomImage() {
   if (!subTitle.classList.contains('clicked')) {
     return; // Return early if the subtitle has not been clicked
   }
+
   const mouseX = lastMouseX;
   const mouseY = lastMouseY;
 
@@ -64,7 +90,6 @@ function getRandomImage() {
       const originalImage = new Image();
       originalImage.src = randomImage;
 
-
       originalImage.addEventListener('load', () => {
         const originalImageRatio = originalImage.width / originalImage.height;
 
@@ -75,8 +100,9 @@ function getRandomImage() {
           newImage.style.width = 'auto';
           newImage.style.height = imageHeight + 'px';
         }
-        const randomCenterX = Math.random() * (screenWidth - imageWidth/3) - (imageWidth / 6);
-        const randomCenterY = Math.random() * (screenHeight - imageHeight/3) - (imageHeight / 6);
+
+        const randomCenterX = Math.random() * (screenWidth - imageWidth / 3) - imageWidth / 6;
+        const randomCenterY = Math.random() * (screenHeight - imageHeight / 3) - imageHeight / 6;
 
         const left = randomCenterX - newImage.clientWidth / 2;
         const top = randomCenterY - newImage.clientHeight / 2;
@@ -84,10 +110,16 @@ function getRandomImage() {
         newImage.style.left = left + 'px';
         newImage.style.top = top + 'px';
 
+        newImage.addEventListener('click', () => {
+          enterZoomMode(newImage.src);
+        });
+
         if (title.classList.contains('faded-out')) {
           artInspiration.appendChild(newImage); // Add the new image to the DOM after the title has faded out
         }
       });
+
+      newImage.addEventListener('error', handleImageError); // Add error event listener
 
       imageQueue.push(newImage); // Add the new image to the queue
 
@@ -107,7 +139,7 @@ function handleMouseMove(event) {
 
   const distance = calculateDistance(lastMouseX, lastMouseY, mouseX, mouseY);
 
-  if (distance >= mouseSensitivity) {
+  if (distance >= mouseSensitivity && subTitle.classList.contains('clicked')) {
     lastMouseX = mouseX;
     lastMouseY = mouseY;
 
@@ -125,6 +157,7 @@ function handleMouseStop() {
     mouseMoving = false;
   }
 }
+
 function fadeOutTitle() {
   if (subTitle.classList.contains('clicked')) {
     title.classList.remove('fade-in');
@@ -132,14 +165,26 @@ function fadeOutTitle() {
     // Wait 3 seconds then add faded-out class
     setTimeout(() => {
       title.classList.add('faded-out');
-    }
-      , 1000);
+    }, 1000);
   }
 }
+
+artInspiration.addEventListener('click', (event) => {
+  const clickedImage = event.target.closest('.added-image');
+  if (clickedImage) {
+    enterZoomMode(clickedImage.src);
+  }
+});
 
 window.addEventListener('mousemove', debounce(handleMouseMove, TimeDelay));
 window.addEventListener('mouseout', handleMouseStop);
 window.addEventListener('blur', handleMouseStop);
+window.addEventListener('click', (event) => {
+  const zoomedContainer = event.target.closest('.zoomed-image');
+  if (!zoomedContainer) {
+    exitZoomMode();
+  }
+}); // Add click event listener to the body to exit zoom mode when clicked outside the image
 
 subTitle.addEventListener('click', () => {
   subTitle.classList.add('clicked');
@@ -151,6 +196,13 @@ subTitle.addEventListener('click', () => {
   }, 1000);
 });
 
+function handleImageError(event) {
+  const errorImage = event.target;
+  if (errorImage.parentNode) {
+    errorImage.parentNode.removeChild(errorImage); // Remove the image from the DOM if it fails to load
+  }
+}
+
 // Debounce function to limit the frequency of event handling
 function debounce(callback, delay) {
   let timerId;
@@ -161,3 +213,15 @@ function debounce(callback, delay) {
     }, delay);
   };
 }
+
+// Add click event listeners to existing images
+function addClickEventListenersToImages() {
+  const existingImages = Array.from(artInspiration.querySelectorAll('.added-image'));
+  existingImages.forEach(image => {
+    image.addEventListener('click', () => {
+      enterZoomMode(image.src);
+    });
+  });
+}
+
+addClickEventListenersToImages();
