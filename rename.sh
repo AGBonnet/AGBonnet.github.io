5d4a5ec699db9060d6caa48fc50c55b99469a291
AGBonnet <abonnet04@gmail.com>
2023-06-16 01:42:53 +0200

    Updating images

--- rename.sh
+++ rename.sh
@@ -6,4 +6,16 @@ for file in VisualFlow/*; do
     mv "$file" "VisualFlow/${new_name}"
     counter=$((counter + 1))
 done
+
+# Go to the VisualFlow directory
+cd VisualFlow
+
+# Rename files with uppercase extensions to lowercase
+for file in *.[JjPpEeGgNnPpWw][PpNnEeGg]*; do
+    newname=$(echo "$file" | tr '[:upper:]' '[:lower:]')
+    mv "$file" "$newname"
+done
+
+cd ..
+
 find VisualFlow/ -type f -exec basename {} \; | jq -R '.' | jq -s '{"images": .}' > images.json
--- scripts/poetry.js
+++ scripts/poetry.js
@@ -1,25 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const lines = document.querySelectorAll('.line');
-  
-    function checkFadeIn() {
-      lines.forEach((line) => {
-        const rect = line.getBoundingClientRect();
-        const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
-  
-        if (isVisible) {
-          line.classList.add('fade-in');
-        } else {
-          line.classList.remove('fade-in');
-        }
-      });
-    }
-  
-    function startFadeInTimer() {
-      checkFadeIn();
-      setInterval(checkFadeIn, 10); // Adjust the interval time as needed
-    }
-  
-    window.addEventListener('scroll', startFadeInTimer);
-    window.addEventListener('resize', startFadeInTimer);
-  });
-  
+  const lines = document.querySelectorAll('.line');
+
+  function updateFadeInOut() {
+
+    lines.forEach((line) => {
+      const rect = line.getBoundingClientRect();    // --> size of line and its position relative to the viewport
+      const lineCenter = rect.top + rect.height / 2; // --> center of line relative to viewport
+      const distFromCenter = Math.abs(window.innerHeight / 2 - lineCenter); // --> distance from center of line to center of viewport
+      // Make opacity 1 when line is at center of viewport, and 0 when line is at top or bottom of viewport, with quadratic easing
+      const opacity = 1 - Math.pow(distFromCenter / (window.innerHeight / 2), 2);
+      
+      line.style.opacity = opacity;
+    });
+  }
+
+  function startFadeInOut() {
+    updateFadeInOut();
+  }
+
+  window.addEventListener('scroll', startFadeInOut);
+  window.addEventListener('resize', startFadeInOut);
+});
